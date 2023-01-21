d553bbfa965374aceae72a2246fa6b7814b108c3
poptsov <alexey.poptsov@mail.ru>
2023-01-21 22:36:18 +0300

    0.6 add video

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,8 @@ from flask_migrate import Migrate
 
 from flask_bootstrap import Bootstrap
 
+import cv2
+
 # app = Flask(__name__,
 #             static_folder='/path/to/static',
 #             template_folder='/path/to/templates')
@@ -18,5 +20,11 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
 detector = Detector(model_IDs = [0,1,2], path_images=app.config['UPLOAD_FOLDER'])
+
+video = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier()
+
+# Load the pretrained model
+face_cascade.load(cv2.samples.findFile("static/haarcascade_frontalface_alt.xml"))
 
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -6,15 +6,16 @@ from app import detector
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'jfif'])
 
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html", title='Home Page')
+
 
 @app.route('/detectnow')
 def detectnow():
@@ -23,13 +24,13 @@ def detectnow():
 
 @app.route('/upload_image', methods=['POST'])
 def upload_image():
-    #flash(os.path.join(app.config['UPLOAD_FOLDER']))
+    # flash(os.path.join(app.config['UPLOAD_FOLDER']))
     if 'file' not in request.files:
-        #flash('No file part')
+        # flash('No file part')
         return redirect(request.url)
     file = request.files['file']
     if file.filename == '':
-        #flash('No image selected for uploading')
+        # flash('No image selected for uploading')
         return redirect(request.url)
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
@@ -45,16 +46,16 @@ def upload_image():
         for i, model_dict in enumerate(detector.models):
             context['models'].append(model_dict)
 
-
         return render_template('detectnow.html', filename=filename, context=context)
     else:
-        #flash('Allowed image types are -> png, jpg, jpeg, gif')
+        # flash('Allowed image types are -> png, jpg, jpeg, gif')
         return redirect(request.url)
+
 
 @app.route('/display/<filename>')
 def display_image(filename):
     # print('display_image filename: ' + filename)
-    return redirect(url_for('static', filename='uploads/'+filename), code=301)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
 
 @app.route('/settings/all')
@@ -62,16 +63,47 @@ def settings():
     context = {}
     context['title'] = 'Detector parameters:'
     context['parameters'] = []
-    context['parameters'].append({'name': 'CUDA', 'value': detector.device_properties,})
+    context['parameters'].append({'name': 'CUDA', 'value': detector.device_properties, })
     for i, model_dict in enumerate(detector.models):
         context['parameters'].append({'name': f'Model {i}', 'value': model_dict['name'], })
     context['parameters'].append({'name': 'score threshold', 'value': detector.score_threshold, })
     return render_template("settings.html", context=context)
+
 
 @app.route('/table')
 def table():
     return render_template("table.html", title='Settings page')
 
 
+def gen(video):
+    while True:
+        success, image = video.read()
+        frame_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        frame_gray = cv2.equalizeHist(frame_gray)
+
+        faces = face_cascade.detectMultiScale(frame_gray)
+
+        for (x, y, w, h) in faces:
+            center = (x + w // 2, y + h // 2)
+            cv2.putText(image, "X: " + str(center[0]) + " Y: " + str(center[1]), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                        (255, 0, 0), 3)
+            image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            faceROI = frame_gray[y:y + h, x:x + w]
+        ret, jpeg = cv2.imencode('.jpg', image)
+
+        frame = jpeg.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
+@app.route('/video_feed')
+def video_feed():
+    # Set to global because we refer the video variable on global scope,
+    # Or in other words outside the function
+    global video
+
+    # Return the result on the web
+    return Response(gen(video),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
